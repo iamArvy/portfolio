205fae91674d7253b0291a372cf9296cfa837c3a
iamarvy <okeseyi5@gmail.com>
2025-08-17 01:22:07 +0100

    feat: removed bio and made the section flex row with just image, name, role and resume

--- components/profile/Section.vue
+++ components/profile/Section.vue
@@ -3,28 +3,25 @@ const { profile } = useContent();
 const { appName, image } = useAppConfig()
 </script>
 <template>
-  <div v-if="profile" class="flex flex-col lg:grid grid-cols-2">
-    <div class="flex items-center gap-4">
+  <div v-if="profile" class="flex flex-row space-x-7">
       <NuxtImg
         :src="image"
         :alt="appName"
-        width="100"
-        height="100"
+        width="150"
+        height="150"
         class="rounded-sm"
         placeholder
         quality="100"
         fit="cover"
       />
-      <div class="flex flex-col space-y-2">
-        <h1 class="text-lg lg:text-3xl font-bold text-sidebar-primary">
+      <div class="flex flex-col sm:space-y-2 col-span-2">
+        <h1 class="text-lg sm:text-3xl md:text-4xl lg:text-5xl font-bold text-sidebar-primary">
           {{ appName }}
         </h1>
-        <h3 class="text-sm">
+        <h3 class="text-sm sm:text-base md:text-lg lg:text-xl">
           {{ profile.roleName }}
         </h3>
         <ResumeButton v-if="profile.resume" :file="profile.resume" />
       </div>
     </div>
-    <p class="text-sm lg:text-base p-2">{{ profile.bio }}</p>
-  </div>
 </template>
